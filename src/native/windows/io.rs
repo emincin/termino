0caf3ccbf592b9ad9ba7280b2620c753876cbf96
emincin.windows <emincin.life@gmail.com>
2025-11-20 15:50:43 +0800

    Update io.rs

--- src/native/windows/io.rs
+++ src/native/windows/io.rs
@@ -2,6 +2,12 @@ unsafe extern "system" {
     #[link_name = "GetStdHandle"]
     unsafe fn get_std_handle(id: u32) -> usize;
 
+    #[link_name = "GetConsoleOutputCP"]
+    unsafe fn get_console_output_cp() -> u32;
+
+    #[link_name = "SetConsoleOutputCP"]
+    unsafe fn set_console_output_cp(code_page: u32) -> i32;
+
     #[link_name = "WriteConsoleA"]
     unsafe fn write_console(
         handle: usize,
@@ -13,9 +19,14 @@ unsafe extern "system" {
 }
 
 const STD_OUTPUT_HANDLE: u32 = -11i32 as u32;
+const CP_UTF8: u32 = 65001;
 
 pub fn print(s: &str) {
     unsafe {
+        let code_page: u32 = get_console_output_cp();
+        if code_page != CP_UTF8 {
+            set_console_output_cp(CP_UTF8);
+        }
         let handle: usize = get_std_handle(STD_OUTPUT_HANDLE);
         let mut written: u32 = 0;
         write_console(handle, s.as_ptr(), s.len() as u32, &mut written, 0);
