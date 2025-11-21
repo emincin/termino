190890b0980d68cb7d27bca3de5c08ddc875580e
emincin.macos <emincin.life@gmail.com>
2025-11-21 23:41:35 +0800

    Update io.rs

--- src/native/unix/io.rs
+++ src/native/unix/io.rs
@@ -1,7 +1,10 @@
 unsafe extern "C" {
+    unsafe fn read(fd: i32, buf: *mut u8, len: usize) -> isize;
+
     unsafe fn write(fd: i32, buf: *const u8, len: usize) -> isize;
 }
 
+const STDIN_FILENO: i32 = 0;
 const STDOUT_FILENO: i32 = 1;
 
 pub fn print(s: &str) {
