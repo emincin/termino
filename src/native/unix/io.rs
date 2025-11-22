c11dc807eaaae2942133086a96a8f1f16ebe1c49
emincin.macos <emincin.life@gmail.com>
2025-11-23 02:22:10 +0800

    Update io.rs

--- src/native/unix/io.rs
+++ src/native/unix/io.rs
@@ -4,11 +4,24 @@ unsafe extern "C" {
     unsafe fn write(fd: i32, buf: *const u8, len: usize) -> isize;
 }
 
+const ERROR: isize = -1;
 const STDIN_FILENO: i32 = 0;
 const STDOUT_FILENO: i32 = 1;
 
 pub fn print(s: &str) {
     unsafe {
         write(STDOUT_FILENO, s.as_ptr(), s.len());
+    }
+}
+
+pub fn read_string(capacity: usize) -> String {
+    unsafe {
+        let mut s: String = String::with_capacity(capacity);
+        let ret: isize = read(STDIN_FILENO, s.as_mut_ptr(), capacity);
+        if ret == ERROR {
+            return String::new();
+        }
+        s.as_mut_vec().set_len(ret as usize);
+        return s;
     }
 }
