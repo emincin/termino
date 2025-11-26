93c96b3e4b402cf7c5978779e9f741d683f7b603
emincin.windows <emincin.life@gmail.com>
2025-11-26 20:27:38 +0800

    Update raw.rs

--- examples/raw.rs
+++ examples/raw.rs
@@ -3,4 +3,11 @@ use termino::io::*;
 fn main() {
     let ok: bool = enable_raw_mode();
     println!("Raw mode: {}", ok);
+    while ok {
+        let input: String = read_string(1024);
+        if input.eq_ignore_ascii_case("q") {
+            break;
+        }
+        print(&input);
+    }
 }
