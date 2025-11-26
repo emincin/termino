f42f86c41847a5c5fcf47d3b69d2dd3d8f4d3f2e
emincin.macos <emincin.life@gmail.com>
2025-11-26 15:05:12 +0800

    Update io.rs

--- src/native/unix/io.rs
+++ src/native/unix/io.rs
@@ -1,4 +1,6 @@
 unsafe extern "C" {
+    unsafe fn isatty(fd: i32) -> i32;
+
     unsafe fn read(fd: i32, buf: *mut u8, len: usize) -> isize;
 
     unsafe fn write(fd: i32, buf: *const u8, len: usize) -> isize;
@@ -8,14 +10,24 @@ const ERROR: isize = -1;
 const STDIN_FILENO: i32 = 0;
 const STDOUT_FILENO: i32 = 1;
 
-pub fn print(s: &str) {
+pub fn print(s: &str) -> usize {
     unsafe {
-        write(STDOUT_FILENO, s.as_ptr(), s.len());
+        if isatty(STDOUT_FILENO) == 0 {
+            return 0;
+        }
+        let ret: isize = write(STDOUT_FILENO, s.as_ptr(), s.len());
+        if ret < 0 {
+            return 0;
+        }
+        return ret as usize;
     }
 }
 
 pub fn read_string(capacity: usize) -> String {
     unsafe {
+        if isatty(STDIN_FILENO) == 0 {
+            return String::new();
+        }
         let mut s: String = String::with_capacity(capacity);
         let ret: isize = read(STDIN_FILENO, s.as_mut_ptr(), capacity);
         if ret == ERROR {
