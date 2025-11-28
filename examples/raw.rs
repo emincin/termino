d7e70de659e07717e161572cf230b9774f381f45
emincin.macos <emincin.life@gmail.com>
2025-11-29 02:01:30 +0800

    Update raw.rs

--- examples/raw.rs
+++ examples/raw.rs
@@ -1,4 +1,5 @@
 use termino::io::*;
+use termino::utils::*;
 
 fn main() {
     let ok: bool = enable_raw_mode();
@@ -7,6 +8,12 @@ fn main() {
         let input: String = read_string(1024);
         if input.eq_ignore_ascii_case("q") {
             break;
+        }
+        if input.len() == 1 && is_all_control(&input) {
+            for c in input.chars() {
+                let s: String = (c as u32).to_string();
+                print(&s);
+            }
         }
         print(&input);
     }
