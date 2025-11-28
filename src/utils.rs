e5e37caba0e0f8ae19fd8702da971b6646f2f77f
emincin.macos <emincin.life@gmail.com>
2025-11-29 01:45:42 +0800

    Update utils.rs

--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,10 @@
+pub fn is_all_control(s: &str) -> bool {
+    if s.is_empty() {
+        return false;
+    }
+    return s.chars().all(|c: char| c.is_ascii_control());
+}
+
 pub fn is_all_digits(s: &str) -> bool {
     if s.is_empty() {
         return false;
