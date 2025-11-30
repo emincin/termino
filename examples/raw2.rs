26a5ea755b808a7338ed39552b543551279273de
emincin.windows <emincin.life@gmail.com>
2025-11-30 15:25:18 +0800

    Update raw2.rs

--- examples/raw2.rs
+++ examples/raw2.rs
@@ -5,5 +5,16 @@ fn main() {
     println!("Raw mode: {}", ok);
     while ok {
         let input: String = read_string(1024);
+        if input.len() == 1 && input.bytes().all(|b: u8| b == 27) {
+            break;
+        }
+        print!("{:?} => ", input.as_bytes());
+        for (i, c) in input.chars().enumerate() {
+            if i != input.chars().count() - 1 {
+                print!("{}, ", c as u32);
+            } else {
+                println!("{}", c as u32);
+            }
+        }
     }
 }
