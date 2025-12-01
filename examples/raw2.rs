efd976f4b3e44b83103750090f2c69a0c882a4e4
emincin.windows <emincin.life@gmail.com>
2025-12-01 23:58:17 +0800

    Update raw2.rs

--- examples/raw2.rs
+++ examples/raw2.rs
@@ -5,6 +5,9 @@ fn main() {
     println!("Raw mode: {}", ok);
     while ok {
         let input: String = read_string(1024);
+        if input.is_empty() {
+            continue;
+        }
         if input.len() == 1 && input.bytes().all(|b: u8| b == 27) {
             break;
         }
